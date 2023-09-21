d274c9aa565bd68c81d96d28d922b010d5386e44
Jens Neuhalfen <jens@neuhalfen.name>
2023-09-21 21:48:33 +0200

    force NUM_LOCK off
    Under MacOS num-lock leads to strange behavior when pressing ":"

--- neuhalje.c
+++ neuhalje.c
@@ -1,4 +1,13 @@
-// Copyright 2022 @filterpaper
+// Copyright 2022 @jens neuhalfen
 // SPDX-License-Identifier: GPL-2.0+
 
 #include "neuhalje.h"
+#include QMK_KEYBOARD_H
+
+bool led_update_user(led_t led_state) {
+    // force NUM LOCK OFF (it makes ":" under macos emacs beahave wierd)
+    if (led_state.num_lock) {
+        tap_code(KC_NUM_LOCK);
+    }
+    return true;
+}
